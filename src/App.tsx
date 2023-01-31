a8fc886994fd0945cdb1868c06b1951d12d9e676
Mmmustapha42 <111079800+Mmmustapha42@users.noreply.github.com>
2023-01-31 01:30:25 +0100

    shopping cart

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import { Route, Routes } from "react-router-dom"
+import { Container } from "react-bootstrap"
+import { Home } from "./pages/Home"
+import { About } from "./pages/About"
+import { Store } from "./pages/Store"
+import { Navbar } from "./components/Navbar"
+import { ShoppingCartProvider } from "./context/ShoppingCartContext"
+import { Children } from "react"
+
+
+
+function App() {
+  return (
+    
+    <ShoppingCartProvider >
+    <Navbar/>
+    <Container className="mb-4">
+    <Routes>
+      <Route path="/" element={<Home/>}></Route>
+      <Route path="/store" element={<Store/>}></Route>
+      <Route path="/about" element={<About/>}></Route>
+    </Routes>
+    </Container>
+    </ShoppingCartProvider>
+  )
+}
+
+export default App
